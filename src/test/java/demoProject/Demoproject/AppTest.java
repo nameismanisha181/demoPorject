d2669d41dda4cf6099ad91ab8ae5184ee953005d
manisha <manisha.lipare17@gmail.com>
2022-12-14 06:32:13 -0500

    first commit

---
+++ src/test/java/demoProject/Demoproject/AppTest.java
@@ -0,0 +1,20 @@
+package demoProject.Demoproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/demoProject/Demoproject/tester1.java
@@ -0,0 +1,11 @@
+package demoProject.Demoproject;
+
+import org.testng.annotations.Test;
+
+@Test
+public class tester1 {
+public void demo1()
+{
+	System.out.println("demo1");
+}
+}
